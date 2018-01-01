de35192d1d516674e6b87d4be42d886b7fce0870
peaches <pablo.chois@gmail.com>
2018-01-01 12:08:17 -0500

    updated index

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static("App"));
+app.use(express.static('dist'));
 
 app.get('/', function (req, res) {
     res.redirect('/');
